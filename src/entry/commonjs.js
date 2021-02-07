44bf0b01ba718a6a831e8174ae1e61bfc79cda65
Bobby Powers <bobbypowers@gmail.com>
2021-02-06 21:51:01 -0800

    directly import jQuery for commonjs

--- src/entry/commonjs.js
+++ src/entry/commonjs.js
@@ -1,7 +1,9 @@
+const jq = require('jquery');
+
 module.exports = function(jQuery) {
   if (!jQuery) throw 'MathQuill requires jQuery 1.5.2+ to be loaded first';
 
   {SOURCE}
 
   return MathQuill;
-};
+}(jq);
